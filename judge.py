2527f88f0a12462a44da1bcfd641349c802d0ec7
matsui <matsui@posl.ait.kyushu-u.ac.jp>
2020-04-03 02:06:22 +0900

    クラスに切り分け＆ログインモジュールの使用

--- judge.py
+++ judge.py
@@ -2,12 +2,108 @@ import sys, os
 import subprocess
 import config
 from utils.pycolor import PyColor
+from utils.login import AtConnector
 from argparse import ArgumentParser
 from bs4 import BeautifulSoup
 import requests
 
 PYTHON_ID = 3023
 PYPY_ID = 3510
+
+class Judge:
+    def __init__(self, contest_type, contest_id, prob_type):
+        self.contest_type = contest_type
+        self.contest_id = contest_id
+        self.prob_type = prob_type
+        if not config.ATCODER_DIR_PATH == None:
+            atcoder_dir_path = config.ATCODER_DIR_PATH
+        else:
+            atcoder_dir_path = './'
+        target_contest = self.atcoder_dir_path + contest_type + '/' + contest_id
+        self.test_target = target_contest + '/' + prob_type + '.py'
+        self.tests_dir   = target_contest + '/tests/' + prob_type + '/'
+
+        test_files = sorted(os.listdir(self.tests_dir))
+        self.test_cases = []
+        for i in range(0, len(test_files), 2):
+            # 00_input.txt, 00_output.txtをまとめてtest_caseとする
+            test_case = (test_files[i], test_files[i+1])
+            self.test_cases.append(test_case)
+
+    def test(self, diff=None, verbose=False):
+        pc = PyColor()
+        print('test num: {}'.format(len(self.test_cases)))
+        for test in self.test_cases:
+            test_input = test[0]
+            test_output = test[1]
+            prefix = test_input[:2]
+            # prefixの２桁目が0の場合はsampleテストケース,1の場合は追加したテストケースを表す
+            if prefix[0] == '0':
+                pc.pprint('sample_case' + prefix + ' => ', end='', bold=True)
+            else:
+                pc.pprint('additional_case' + prefix + ' => ', end='', bold=True)
+            # テスト実行
+            # ex: python <atcoder-dir-path>/abc/134/a.py < <atcoder-dir-path>/abc/134/tests/A/00_input.txt
+            command = ['python', self.test_target, '<', self.tests_dir + test_input]
+            std = subprocess.run(' '.join(command), stdout=subprocess.PIPE, stderr=subprocess.STDOUT, shell=True)
+
+            # Judge!
+            with open(self.tests_dir + test_output) as f:
+                expected = f.read().rstrip()
+                actual = std.stdout.decode('utf-8').rstrip()
+                # --diffオプションを指定した場合は誤差を判定する
+                if diff:
+                    is_correct = (abs(float(expected) - float(actual)) < args.diff)
+                else:
+                    is_correct = (expected == actual)
+            if verbose:
+                with open(self.tests_dir + test_input) as f:
+                    input_val = f.read().rstrip()
+            if is_correct:
+                pc.pprint('OK', color='green')
+                if verbose:
+                    print('[input]')
+                    print('{}'.format(input_val))
+                    print('[output]')
+                    print('{}'.format(actual))
+            else:
+                pc.pprint('NG', color='r')
+                if verbose:
+                    print('[input]')
+                    print('{}'.format(input_val))
+                pc.pprint('[expected]', color='g')
+                pc.pprint('{}'.format(expected), color='g')
+                pc.pprint('[actual]', color='r')
+                pc.pprint('{}'.format(actual), color='r')
+        return is_correct
+
+    def submit(self, submit_lang_id):
+        ac = AtConnector()
+        ac.login()
+        with open(self.test_target, 'r') as f:
+            submit_code = f.read()
+        submit_url = \
+            'https://atcoder.jp/contests/' \
+            + self.contest_type + self.contest_id \
+            + '/submit'
+        html = ac.session.get(submit_url)
+        html.raise_for_status()
+        soup = BeautifulSoup(html.text, 'lxml')
+        csrf_token = soup.find(attrs={'name': 'csrf_token'}).get('value')
+        task_screen_name = self.contest_type + self.contest_id + '_' + self.prob_type
+        submit_info = {
+            "data.TaskScreenName": task_screen_name,
+            "csrf_token": csrf_token,
+            "data.LanguageId": submit_lang_id,
+            "sourceCode": submit_code
+        }
+        result = ac.session.post(submit_url, data=submit_info)
+        result.raise_for_status()
+        if result.status_code == 200:
+            print("Submitted!")
+        else:
+            print("Error in submitting...")
+            exit(1)
 
 if __name__ == '__main__':
     example = """
@@ -44,125 +140,15 @@ if __name__ == '__main__':
 
     args = argparser.parse_args()
 
-    contest_type = args.contest_type
-    contest_id = args.contest_id
-    problem_type = args.problem_type
+    judge = Judge(args.contest_type,
+                  args.contest_id,
+                  args.problem_type)
 
-    # config.pyにatcoderディレクトリの指定がない場合はカレントディレクトリを指定
-    if not config.ATCODER_DIR_PATH == None:
-        atcoder_dir_path = config.ATCODER_DIR_PATH
-    else:
-        atcoder_dir_path = './'
+    result = judge.test(verbose=args.verbose, diff=args.diff)
 
-    pc = PyColor()
-
-    target_contest = atcoder_dir_path + contest_type + '/' + contest_id
-    test_target = target_contest + '/' + problem_type + '.py'
-    tests_dir   = target_contest + '/tests/' + problem_type + '/'
-
-    test_files = sorted(os.listdir(tests_dir))
-    test_cases = []
-    for i in range(0, len(test_files), 2):
-        # 00_input.txt, 00_output.txtをまとめてtest_caseとする
-        test_case = (test_files[i], test_files[i+1])
-        test_cases.append(test_case)
-
-    print('test num: {}'.format(len(test_cases)))
-
-    for test in test_cases:
-        test_input = test[0]
-        test_output = test[1]
-        prefix = test_input[:2]
-        # prefixの２桁目が0の場合はsampleテストケース,1の場合は追加したテストケースを表す
-        if prefix[0] == '0':
-            pc.pprint('sample_case' + prefix + ' => ', end='', bold=True)
-        else:
-            pc.pprint('additional_case' + prefix + ' => ', end='', bold=True)
-
-        # テスト実行
-        # ex: python <atcoder-dir-path>/abc/134/a.py < <atcoder-dir-path>/abc/134/tests/A/00_input.txt
-        command = ['python', test_target, '<', tests_dir + test_input]
-        std = subprocess.run(' '.join(command), stdout=subprocess.PIPE, stderr=subprocess.STDOUT, shell=True)
-
-        # Judge!
-        with open(tests_dir + test_output) as f:
-            expected = f.read().rstrip()
-            actual = std.stdout.decode('utf-8').rstrip()
-
-            # --diffオプションを指定した場合は誤差を判定する
-            if args.diff:
-                is_correct = (abs(float(expected) - float(actual)) < args.diff)
-            else:
-                is_correct = (expected == actual)
-
-        if args.verbose:
-            with open(tests_dir + test_input) as f:
-                input_val = f.read().rstrip()
-
-        if is_correct:
-            pc.pprint('OK', color='green')
-            if args.verbose:
-                print('[input]')
-                print('{}'.format(input_val))
-                print('[output]')
-                print('{}'.format(actual))
-        else:
-            pc.pprint('NG', color='r')
-            if args.verbose:
-                print('[input]')
-                print('{}'.format(input_val))
-            pc.pprint('[expected]', color='g')
-            pc.pprint('{}'.format(expected), color='g')
-            pc.pprint('[actual]', color='r')
-            pc.pprint('{}'.format(actual), color='r')
-
-    # is_correct = True かつ submitオプションがある場合
-    if is_correct and args.submit:
+    if result and args.submit:
         if args.submit == 'p':
             submit_lang_id = PYTHON_ID
         else:
             submit_lang_id = PYPY_ID
-
-        source_path = test_target
-        with open(source_path, 'r') as f:
-            source_code = f.read()
-
-        LOGIN_URL = "https://atcoder.jp/login"
-        # セッション開始
-        session = requests.session()
-        # csrf_token取得
-        r = session.get(LOGIN_URL)
-        s = BeautifulSoup(r.text, 'lxml')
-        csrf_token = s.find(attrs={'name': 'csrf_token'}).get('value')
-        # パラメータセット
-        login_info = {
-            "csrf_token": csrf_token,
-            "username": config.USERNAME,
-            "password": config.PASSWORD
-        }
-        result = session.post(LOGIN_URL, data=login_info)
-        result.raise_for_status()
-        if result.status_code==200:
-            print("Login!")
-        else:
-            print("Failed...")
-            exit(1)
-
-        submit_url = 'https://atcoder.jp/contests/' + contest_type + contest_id + '/submit'
-        html = session.get(submit_url)
-        html.raise_for_status()
-        soup = BeautifulSoup(html.text, 'lxml')
-        csrf_token = soup.find(attrs={'name': 'csrf_token'}).get('value')
-        submit_info = {
-            "data.TaskScreenName": contest_type + contest_id + '_' + problem_type,
-            "csrf_token": csrf_token,
-            "data.LanguageId": submit_lang_id,
-            "sourceCode": source_code
-        }
-        result = session.post(submit_url, data=submit_info)
-        result.raise_for_status()
-
-        if result.status_code == 200:
-            print("Submitted!")
-        else:
-            print("Error in submitting...")
+        judge.submit(submit_lang_id)
