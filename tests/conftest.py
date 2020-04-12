350b2a99859f74447cfa5fe58a6dc7ed7c9f6d60
matsui <matsui@posl.ait.kyushu-u.ac.jp>
2020-04-12 23:37:55 +0900

    configファイルのmock化 config.pyはgit管理していないため，存在していないファイルをimportしないようにmockを用意

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,8 +1,10 @@
 import pytest
 import sys
 from pathlib import Path
+from unittest.mock import Mock
 
 sys.path.append(str(Path(__file__).resolve().parent.parent))
+sys.modules['config'] = Mock()
 import config
 
 @pytest.fixture(scope="session", autouse=True)
