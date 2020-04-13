dd83d9f0f010b06743746b09ec90957050532ebd
matsui <matsui@posl.ait.kyushu-u.ac.jp>
2020-04-13 11:48:04 +0900

    不要なimportを削除

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,9 +1,7 @@
 import pytest
 import sys
-from pathlib import Path
 from unittest.mock import Mock
 
-sys.path.append(str(Path(__file__).resolve().parent.parent))
 sys.modules['config'] = Mock()
 import config
 
