0118e8df70c465277eba0daeb038a4e305b36efa
matsui <matsui@posl.ait.kyushu-u.ac.jp>
2020-04-14 12:26:27 +0900

    add force submit option

--- judge.py
+++ judge.py
@@ -3,14 +3,16 @@ from argparse import ArgumentParser
 
 if __name__ == '__main__':
     example = """
-    ex1: abc134のd問題をテスト
-        python {0} abc 134 d
-    ex2: abc142のa問題をテスト（差が1e-6以内かどうか）
-        python {0} abc 142 a -d 1e-6e
-    ex3: abc142のa問題がテスト通過したら提出（python3）
-        python {0} abc 142 a -s p
-    ex4: abc142のa問題がテスト通過したら提出（pypy3）
-        python {0} abc 142 a -s pp
+    ex1: abc123のd問題をテスト
+        python {0} abc 123 d
+    ex2: abc123のa問題をテスト（差が1e-6以内かどうか）
+        python {0} abc 123 a -d 1e-6e
+    ex3: abc123のa問題がテスト通過したら提出（python3）
+        python {0} abc 123 a -s p
+    ex4: abc123のa問題がテスト通過したら提出（pypy3）
+        python {0} abc 123 a -s pp
+    ex5: テストの成否を無視して提出
+        python {0} abc 123 a -s p -f
     """.format(__file__)
 
     argparser = ArgumentParser(usage=example)
@@ -33,6 +35,9 @@ if __name__ == '__main__':
                            type=str,
                            choices=['p', 'pp'],
                            help='テストに全て通過した場合に提出する')
+    argparser.add_argument('-f', '--force',
+                           action='store_true',
+                           help='テストの成否を無視して強制的に提出する')
 
     args = argparser.parse_args()
 
@@ -42,5 +47,5 @@ if __name__ == '__main__':
 
     result = judge.test(verbose=args.verbose, diff=args.diff)
 
-    if result and args.submit:
+    if (result or args.force) and args.submit:
         judge.submit(args.submit)
