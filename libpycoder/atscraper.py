bfd9890b210e069a1299dcf8d8191b977491aa3f
matsui <matsui@posl.ait.kyushu-u.ac.jp>
2020-04-22 16:00:19 +0900

    サンプルケース抽出処理を関数化

--- libpycoder/atscraper.py
+++ libpycoder/atscraper.py
@@ -1,4 +1,4 @@
-from typing import Dict
+from typing import Dict, List, Tuple
 from bs4 import BeautifulSoup as bs
 
 def extract_task_screen_name(html: str, prob_type: str) -> str:
@@ -26,3 +26,32 @@ def extract_prob_links(html: str) -> Dict[str, str]:
         link = item.get('href') # ex: /contests/abc160/tasks/abc160_a
         prob_links[p_type] = link
     return prob_links
+
+def extract_sample_test_cases_from_prob_page(html: str) -> Dict[int, Tuple[str,str]]:
+    """問題ページからサンプルテストケースを抽出する.
+    @param html 問題ページ
+    @return 問題ページのサンプルテストケース.
+        {0: (入力例1,出力例1), 1: (入力例2, 出力例2), ...}といった形式の辞書で返す.
+    """
+    soup = bs(html, 'html5lib')
+    # 英語ページを除外（日本語ページのサンプルケースと重複して取得してしまうのを防ぐため）
+    while soup.find('span', class_='lang-en'):
+        soup.find('span', class_='lang-en').extract()
+    # 入出力の形式欄を除外
+    while soup.find('div', class_='io-style'):
+        soup.find('div', class_='io-style').extract()
+
+    io_samples = []
+    for sec in soup.find_all('section'):
+        # section内の先頭以外のpreは'間違いの例'などサンプルケースでないことがあるため、
+        # 先頭のpreのみを取得する
+        pre = sec.find('pre')
+        # 問題文もsection内にあるが、問題文にはpreタグは存在しないため
+        # preタグの有無でサンプルケースを取得することができる。
+        if pre != None:
+            io_samples.append(pre)
+
+    sample_test_cases = {}
+    for i in range(0, len(io_samples), 2):
+        sample_test_cases[i//2] = (io_samples[i].get_text(), io_samples[i+1].get_text())
+    return sample_test_cases
